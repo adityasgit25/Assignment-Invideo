adea594834b349e9f54c9a989eef56a76f515e03
Aditya-M02 <adityamah2002@gmail.com>
2025-02-26 00:26:02 +0530

    navbar

--- math-eval/src/App.jsx
+++ math-eval/src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Calculator from "./Calculator"; 
-import TexttoShader from "./TextToShader"; 
+import TextToShader from "./TextToShader"; 
+import Navigation from "./Navigation";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Calculator />} />
-      <Route path="/text-to-shader" element={<TexttoShader />} />
-    </Routes>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Calculator />} />
+        <Route path="/text-to-shader" element={<TextToShader />} />
+      </Routes>
+    </>
   );
 }
